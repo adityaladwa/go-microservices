7c5b8b4f6c6654db82669100204c79a2e57b088f
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-06-01 22:45:47 +0530

    Tests for db Service

---
+++ accountservice/dbservice/mockclient.go
@@ -0,0 +1,23 @@
+package dbservice
+
+import (
+	"github.com/adityaladwa/go-microservices/accountservice/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockBoltClient struct {
+	mock.Mock
+}
+
+func (m *MockBoltClient) QueryAccount(accountId string) (model.Account, error) {
+	args := m.Mock.Called(accountId)
+	return args.Get(0).(model.Account), args.Error(1)
+}
+
+func (m *MockBoltClient) OpenBoltDb() {
+
+}
+
+func (m *MockBoltClient) Seed() {
+
+}
